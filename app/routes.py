6f760e9d8fbdb30c64db0c580cc63ba0a9fdce3b
kmi0817 <kmi0817@naver.com>
2022-01-12 17:28:13 +0900

    add route '/loginout' (use session)

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,28 @@
 from app import app
-from flask import render_template
+from flask import render_template, request, session
 import requests
 
 @app.route('/')
 def index() :
-    return render_template('index.html')
+    if 'login' in session :
+        login = True
+    else :
+        login = False
+    return render_template('index.html', login=login)
+
+@app.route('/loginout', methods=['POST', 'DELETE'])
+def loginout() :
+    if request.method == 'POST' :
+        values = request.get_json(force=True)
+        id = values['id']
+        password = values['password']
+
+        if id == "asd@asd.com" and password == "asd" :
+            session['login'] = id
+            return "login successful"
+        else :
+            return "login failed"
+
+    elif request.method == 'DELETE' :
+        session.clear()
+        return "logout successful"
