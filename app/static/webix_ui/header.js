84c4012793a098e2c27f41d01d4bac6e888947af
kmi0817 <kmi0817@naver.com>
2022-01-12 17:27:00 +0900

    delete comments & add before/after login header

--- app/static/webix_ui/header.js
+++ app/static/webix_ui/header.js
@@ -5,7 +5,7 @@ const logo = {
     css: "webix_transparent"
 }
 
-const top_toolbar = {
+const before_login_top_toolbar = {
     view: "toolbar",
     borderless: true,
     elements: [
@@ -16,7 +16,52 @@ const top_toolbar = {
                         view: "button", value: "로그인",
                         width: 100,
                         click: () => {
-                            webix.message("로그인");
+                            $$("login_popup").show();
+                        }
+                    },
+                    {
+                        view: "button", value: "회원가입",
+                        width: 100,
+                        click: () => {
+                            webix.message("회원가입");
+                        }
+                    },
+                    {
+                        view: "button", value: "고객선거",
+                        width: 100,
+                        click: () => {
+                            webix.message("고객선거");
+                        }
+                    }
+                ]
+            }
+        }
+    ]
+}
+
+const after_login_top_toolbar = {
+    view: "toolbar",
+    borderless: true,
+    elements: [
+        {
+            align: "right", body: {
+                cols: [
+                    {
+                        view: "button", value: "로그아웃",
+                        width: 100,
+                        click: () => {
+                            webix.ajax().del("/loginout")
+                            .then((res) => {
+                                var response = res.text();
+                                if (response == "logout successful") {
+                                    location.href="/";
+                                } else {
+                                    webix.message({
+                                        type: "error",
+                                        text: "문제가 발생했습니다."
+                                    });
+                                }
+                            });
                         }
                     },
                     {
@@ -40,63 +85,6 @@ const top_toolbar = {
 }
 
 const main_menu = {
-    align: "center",
-    body: {
-        view: "toolbar",
-        borderless: true,
-        elementsConfig: {
-            autowidth: true,
-            minWidth: 120,
-        },
-        elements: [
-            {
-                id: "hello", view: "button", label: "지식 거래 장터 소개"
-            },
-            {
-                id: "peer-feedback", view: "button", label: "동료평가", popup: "peer_feedback_popup"
-            },
-            {
-                id: "auction", view: "button", value: "옥션",
-            },
-            {
-                id: "market", view: "button", value: "마켓"
-            },
-            {
-                id: "odi", view: "button", value: "ODI"
-            },
-            {
-                id: "idea", view: "button", value: "아이디어 장터"
-            },
-            {
-                id: "NFT", view: "button", value: "NFT 생성"
-            }
-        ]
-    }
-}
-
-// webix.ui({
-//     view: "popup", id: "peer_feedback_popup",
-//     scroll: "disable",
-//     body: {
-//         view: "list",
-//         data: [
-//             {
-//                 id: "register/specialists", value: "전문가 등록"
-//             },
-//             {
-//                 id: "apply/intellectual-asset", value: "지식자산 평가 신청"
-//             },
-//             {
-//                 id: "evaluation-results", value: "평가결과 공개"
-//             },
-//             {
-//                 id: "select/intellctual-asset", value: "지식자산 선별"
-//             }
-//         ]
-//     }
-// });
-
-const main_menu2 = {
     view: "menu", id: "my_menu",
     type: {
         width: 147,
@@ -163,11 +151,26 @@ const before_login_header = {
         logo,
         {
             rows: [
-                top_toolbar,
+                before_login_top_toolbar,
                 {
                     view: "spacer", height: 30
                 },
-                main_menu2
+                main_menu
+            ]
+        }
+    ]
+}
+
+const after_login_header = {
+    cols: [
+        logo,
+        {
+            rows: [
+                after_login_top_toolbar,
+                {
+                    view: "spacer", height: 30
+                },
+                main_menu
             ]
         }
     ]
