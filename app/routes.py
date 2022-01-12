4c8c7f1ed03a46abd66332f2210a67e87d957955
kmi0817 <kmi0817@naver.com>
2022-01-12 17:46:39 +0900

    add every route

--- app/routes.py
+++ app/routes.py
@@ -17,12 +17,68 @@ def loginout() :
         id = values['id']
         password = values['password']
 
-        if id == "asd@asd.com" and password == "asd" :
+        if id == 'asd@asd.com' and password == 'asd' :
             session['login'] = id
-            return "login successful"
+            return 'login successful'
         else :
-            return "login failed"
+            return 'login failed'
 
     elif request.method == 'DELETE' :
         session.clear()
-        return "logout successful"
+        return 'logout successful'
+
+# 지식 거래 장터 소개
+@app.route('/hello')
+def hello() :
+    return '지식 거래 장터 소개'
+
+# 동료 평가
+@app.route('/register/<subject>')
+def register(subject) :
+    if subject == 'specialists' :
+        return '전문가 등록'
+    elif subject == 'seller' :
+        return '판매자 등록'
+    elif subject == 'buyer' :
+        return '구매자 등록'
+
+@app.route('/intellectual-asset/<action>')
+def intellectual_asset(action) :
+    if action == 'apply' :
+        return '지식자산 평가 신청'
+    elif action == 'select' :
+        return '지식자산 선별'
+
+@app.route('/evaluation-results')
+def evaluation_results() :
+    return '평가결과 공개'
+
+# 옥션
+@app.route('/auction')
+def auction() :
+    return "옥션"
+
+# 마켓
+@app.route('/market')
+def market() :
+    return "마켓"
+
+# ODI
+@app.route('/odi')
+def odi() :
+    return "ODI"
+
+# 아이디어 장터 메뉴
+@app.route('/idea/<submenu>')
+def idea(submenu) :
+    if submenu == 'challenge' :
+        return '아이디어 도전'
+    elif submenu == 'basket' :
+        return '아이디어 바구니'
+    elif submenu == 'community' :
+        return '커뮤니티'
+
+# NFT 생성
+@app.route('/nft')
+def nft() :
+    return "NFT 생성"
