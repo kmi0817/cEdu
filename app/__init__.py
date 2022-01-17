db3edbdc60850437bbb35a3a3be75d13a123af36
kmi0817 <kmi0817@naver.com>
2022-01-17 11:59:04 +0900

    update

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,9 @@
 from flask import Flask
+# from flask_pymoongo import PyMongo
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secretKey'
+# app.config['MONGO_URL'] = "mongodb://localhost:27017/app"
+# mongo = PyMongo(app)
+
 from app import routes
