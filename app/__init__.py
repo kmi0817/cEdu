5598af253eed1a6daa4e7f97b15358311ef06b42
kmi0817 <kmi0817@naver.com>
2022-01-19 14:35:46 +0900

    delete PyMongo

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,6 @@
 from flask import Flask
-# from flask_pymoongo import PyMongo
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secretKey'
-# app.config['MONGO_URL'] = "mongodb://localhost:27017/app"
-# mongo = PyMongo(app)
 
 from app import routes
