6f9e095a640ab052a4dcbca36c4aa487a93e88ab
kmi0817 <kmi0817@naver.com>
2022-01-24 15:22:28 +0900

    relocate some routes & add '/evaluation' route

--- app/routes.py
+++ app/routes.py
@@ -25,57 +25,16 @@ def index() :
         login = False
     return render_template('index.html', login=login)
 
-# login, logout
-@app.route('/loginout', methods=['POST', 'DELETE'])
-def loginout() :
-    if request.method == 'POST' :
-        values = request.form
-        try :
-            results = db.users.find_one({ 'email': values['email'] }) # read from db
+# 동료평가
+@app.route('/evaluation')
+def evaluation() :
+    if 'login' in session :
+        login = session['login']
+    else :
+        login = False
+    return render_template('evaluation/evaluation.html', login=login)
 
-            if results['password'] == values['password'] : # check if password matched
-                session['login'] = values['email']
-                return '<script>location.href="/";</script>'
-            else :
-                return '<script>alert("일치하는 회원 정보가 없습니다.");history.go(-1);</script>'
-            
-        except Exception : # if cannot read from db
-            return '<script>alert("로그인에 실패했습니다.");history.go(-1);</script>'
-
-    elif request.method == 'DELETE' :
-        session.pop('login', None)
-        return 'logout successful'
-
-# users - create, delete, update user
-@app.route('/users', methods=['POST', 'DELETE', 'PATCH'])
-def users() :
-    if request.method == 'POST' :
-        values = request.form
-
-        # check if email is duplicated
-        if db.users.find_one({ 'email': values['email'] }) :
-            print(db.users.find_one({ 'email': values['email'] }))
-            return '<script>alert("중복된 이메일입니다.");history.go(-1);</script>'
-
-        # add a new user
-        user = {
-            'email': values['email'],
-            'password': values['password'],
-            'created': datetime.now()
-        }
-        try :
-            results = db.users.insert_one(user)
-            print(f'signup id: {results.inserted_id}')
-            return '<script>location.href="/";</script>'
-        except Exception:
-            return 'singup failed'
-
-    elif request.method == 'DELETE' :
-        return 'delete user'
-    elif request.method == 'PATCH' :
-        return 'update user'
-
-
+# 커뮤니티
 @app.route('/community')
 def community() :
     if 'login' in session :
@@ -244,3 +203,56 @@ def comment_id(id) :
         except Exception as exception :
             print(f'*** error - {exception}')
             return 'falied'
+
+
+
+
+# login, logout
+@app.route('/loginout', methods=['POST', 'DELETE'])
+def loginout() :
+    if request.method == 'POST' :
+        values = request.form
+        try :
+            results = db.users.find_one({ 'email': values['email'] }) # read from db
+
+            if results['password'] == values['password'] : # check if password matched
+                session['login'] = values['email']
+                return '<script>location.href="/";</script>'
+            else :
+                return '<script>alert("일치하는 회원 정보가 없습니다.");history.go(-1);</script>'
+            
+        except Exception : # if cannot read from db
+            return '<script>alert("로그인에 실패했습니다.");history.go(-1);</script>'
+
+    elif request.method == 'DELETE' :
+        session.pop('login', None)
+        return 'logout successful'
+
+# users - create, delete, update user
+@app.route('/users', methods=['POST', 'DELETE', 'PATCH'])
+def users() :
+    if request.method == 'POST' :
+        values = request.form
+
+        # check if email is duplicated
+        if db.users.find_one({ 'email': values['email'] }) :
+            print(db.users.find_one({ 'email': values['email'] }))
+            return '<script>alert("중복된 이메일입니다.");history.go(-1);</script>'
+
+        # add a new user
+        user = {
+            'email': values['email'],
+            'password': values['password'],
+            'created': datetime.now()
+        }
+        try :
+            results = db.users.insert_one(user)
+            print(f'signup id: {results.inserted_id}')
+            return '<script>location.href="/";</script>'
+        except Exception:
+            return 'singup failed'
+
+    elif request.method == 'DELETE' :
+        return 'delete user'
+    elif request.method == 'PATCH' :
+        return 'update user'
